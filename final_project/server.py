bfb3dbe17a1631baedd942c09f442b3cef845ec3
tejrajmeher <tejraj.meher@gmail.com>
2021-08-30 18:04:05 +0000

    updated

--- final_project/server.py
+++ final_project/server.py
@@ -22,6 +22,6 @@ def renderIndexPage():
     
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
+    #app.run(debug=True)
 
