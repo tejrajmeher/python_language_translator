1dbe5566f5fe2b4bd995a1f99e4864d83522d98b
tejrajmeher <tejraj.meher@gmail.com>
2021-08-31 13:39:47 +0000

    final codes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
-import json
+""" Module to translate text between english and french """
+
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -18,33 +19,31 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
-    if englishText=='':
+def english_to_french(english_text):
+    """Convert english to French"""
+    if english_text=='':
         print('input text is empty')
         return None
-    
+
     translation = language_translator.translate(
-        text=englishText,
+        text=english_text,
         model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
-def frenchToEnglish(frenchText):
-    if frenchText=='':
+def french_to_english(french_text):
+    """COnvert French to English"""
+    if french_text=='':
         print('input text is empty')
         return None
 
     translation = language_translator.translate(
-        text=frenchText,
+        text=french_text,
         model_id='fr-en').get_result()
-    englishText = translation['translations'][0]['translation']
-    return englishText
+    english_text = translation['translations'][0]['translation']
+    return english_text
 
 
 if __name__=='__main__':
-    print(englishToFrench('come here'))
-    print(frenchToEnglish('Venir ici'))
-
-
-
-
+    print(english_to_french('come here'))
+    print(french_to_english('Venir ici'))
--- final_project/server.py
+++ final_project/server.py
@@ -8,13 +8,13 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    tr_text=translator.englishToFrench(textToTranslate)
+    tr_text=translator.english_to_french(textToTranslate)
     return tr_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    tr_text=translator.frenchToEnglish(textToTranslate)
+    tr_text=translator.french_to_english(textToTranslate)
     return tr_text
 
 @app.route("/")
