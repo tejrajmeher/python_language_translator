25811a40c5099bdd02e37e57657deee636253ea1
tejrajmeher <tejraj.meher@gmail.com>
2021-08-31 10:51:42 +0000

    final

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,5 +1,5 @@
 from translator import  *
-
+import unittest
 
 print("\n2 tests for each method")
 print(englishToFrench("what is your name"))
@@ -18,3 +18,17 @@ print(englishToFrench('Hello'))
 print(frenchToEnglish('Bonjour'))
 
 print()
+
+
+class TestenglishToFrench(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(englishToFrench("what is your name"), 'Quel est votre nom?') 
+        self.assertNotEqual(englishToFrench("what is your name"), 'quel est ') 
+
+class TestfrenchToEnglish(unittest.TestCase): 
+    def test1(self): 
+        self.assertEqual(frenchToEnglish("quel est ton nom"), 'What is your name') 
+        self.assertNotEqual(frenchToEnglish("quel est ton nom"), 'Who are you') 
+        
+
+unittest.main()
