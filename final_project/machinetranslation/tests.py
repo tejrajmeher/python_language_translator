8a1f3242c685167f0471bec8029f07bf3fac5e85
tejrajmeher <tejraj.meher@gmail.com>
2021-08-31 14:26:39 +0000

    final code

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,33 +2,33 @@ from translator import  *
 import unittest
 
 print("\n2 tests for each method")
-print(englishToFrench("what is your name"))
-print(englishToFrench("how are you"))
+print(english_to_french("what is your name"))
+print(english_to_french("how are you"))
 
-print(frenchToEnglish("quel est ton nom"))
-print(frenchToEnglish("Comment allez-vous"))
+print(french_to_english("quel est ton nom"))
+print(french_to_english("Comment allez-vous"))
 
 
 print("\ntest for null input")
-print(englishToFrench(''))
-print(frenchToEnglish(''))
+print(english_to_french(''))
+print(french_to_english(''))
 
 print('\nHello and Bonjour test')
-print(englishToFrench('Hello'))
-print(frenchToEnglish('Bonjour'))
+print(english_to_french('Hello'))
+print(french_to_english('Bonjour'))
 
 print()
 
 
 class TestenglishToFrench(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(englishToFrench("what is your name"), 'Quel est votre nom?') 
-        self.assertNotEqual(englishToFrench("what is your name"), 'quel est ') 
+        self.assertEqual(english_to_french("what is your name"), 'Quel est votre nom?') 
+        self.assertNotEqual(english_to_french("what is your name"), 'quel est ') 
 
 class TestfrenchToEnglish(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(frenchToEnglish("quel est ton nom"), 'What is your name') 
-        self.assertNotEqual(frenchToEnglish("quel est ton nom"), 'Who are you') 
+        self.assertEqual(french_to_english("quel est ton nom"), 'What is your name') 
+        self.assertNotEqual(french_to_english("quel est ton nom"), 'Who are you') 
         
 
 unittest.main()
